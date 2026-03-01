e13643fc5eacd0ca7dc322232b8e7a4b529793d8
Imrozdotpi <writetoimrozsaim@gmail.com>
2026-03-01 17:19:53 +0530

    Stack implementation using arrays

---
+++ ArrayStack.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ArrayStack{
+    static int stack[];
+    static int top = -1;
+    static int size;
+
+    static void push(int data){
+
+    }
+
+    static void pop(){
+
+    }
+
+    static void peep(){
+
+    }
+
+    static void display(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
